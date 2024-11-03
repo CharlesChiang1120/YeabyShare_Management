6f9fba7bfec2c7d0488e027565661c70a92bea42
Charles <charleschiang2343@gmail.com>
2024-11-03 12:32:18 +0800

    V1

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'b17d8a6e-70a0-4d6d-9448-fb86c89e4d75',
+        productNumber: 'Open',
+        paymentStatus: '270',
+        status: '2'
+    },
+    {
+        productName: 'f629b80d-34f1-4a4d-9f3e-3a3f4b6a8508',
+        productNumber: 'Open',
+        paymentStatus: '190',
+        status: '3'
+    },
+    {
+        productName: '4d80d23a-63c6-4c3e-97d5-e78fa5d7e2a0',
+        productNumber: 'Open',
+        paymentStatus: '330',
+        status: '4'
+    },
+    {
+        productName: 'a9f8cbe1-51d2-4b8a-b5c6-3f8727b1c9e9',
+        productNumber: 'Open',
+        paymentStatus: '210',
+        status: '1'
+    },
+    {
+        productName: '8a3d7c52-78b1-4a4e-a7de-4b5f2d5c1f71',
+        productNumber: 'Open',
+        paymentStatus: '250',
+        status: '3'
+    },
+    {
+        productName: 'f60d9b12-6c3f-4e5b-8e02-7a6b1e2f93c3',
+        productNumber: 'Open',
+        paymentStatus: '310',
+        status: '2'
+    },
+]
